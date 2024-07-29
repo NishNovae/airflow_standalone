21d57de60a9c90c876dd4ab9cbc7fb47fbd798ad
NishNovae <raecrowned@gmail.com>
2024-07-29 16:39:36 +0900

    some changes

--- dags/movie.py
+++ dags/movie.py
@@ -5,6 +5,11 @@ from airflow import DAG
 
 from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
+
+from airflow.operators.python import PythonOperator
+
+from pprint import pprint
+
 with DAG(
         'movie',
     default_args={
@@ -13,19 +18,49 @@ with DAG(
         'retry_delay': timedelta(minutes=1)
     },
     description='movie DAG',
-    schedule=timedelta(days=1),
+    schedule="10 2 * * *",
     start_date=datetime(2024, 7, 10),
     catchup=True,
-    tags=['bash', 'movie', 'db'],
+    tags=['api', 'movie', 'amt'],
 ) as dag:
+
+    def get_data(ds, **kwargs):
+        print(ds)
+        print(kwargs)
+        print("=" * 20) 
+        print(f"ds_nodash => {kwargs['ds_nodash']}")
+        print(f"kwargs type => {type(kwargs)}")
+        print("=" * 20)
+
+        # the question
+        from mov.api.call import save2df, list2df, get_key, req, gen_url
+        key = get_key()
+        print(f"MOVIE_API_KEY => {key}")
+        YYYYMMDD = kwargs['ds_nodash']
+        df = save2df(YYYYMMDD)
+        print(df.head(5))
+
+
+    def print_context(ds=None, **kwargs):
+        """Print the Airflow context and ds variable from the context."""
+        print("::group::All kwargs")
+        pprint(kwargs)
+        print("::endgroup::")
+        print("::group::Context variable ds")
+        print(ds)
+        print("::endgroup::")
+        return "Whatever you return gets printed in the logs"
+
+    run_this = PythonOperator(
+        task_id="print_the_context", 
+        python_callable=print_context
+    )
 
     task_start = EmptyOperator(task_id = 'start')
 
-    task_getdata = BashOperator(
+    task_getdata = PythonOperator(
         task_id = 'get_data',
-        bash_command='''
-            date
-        '''
+        python_callable=get_data
     )
 
     task_savedata = BashOperator(
@@ -40,5 +75,5 @@ with DAG(
 
 
     # DAG flow
-    task_start >> task_getdata >> task_savedata
-    task_savedata >> task_end
+    task_start >> task_getdata >> task_savedata >> task_end
+    task_start >> run_this >> task_end
